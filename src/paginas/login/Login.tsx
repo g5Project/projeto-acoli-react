51273aca2dd531693b4dc5f8c05d4628f7505f7a
Lihsolive <liliamoliveira13@gmail.com>
2023-04-14 20:51:24 -0300

    estilizando os links

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -6,49 +6,18 @@ import "./Login.css";
 
 function Login() {
   return (
-    <Grid
-      className="container"
-      container
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-    >
+    <Grid className="container" container direction="row" justifyContent="center" alignItems="center">
       <Grid className="container-form" alignItems="center" xs={6}>
         <Box className="container-inputs" paddingX={20}>
           <form>
-            <Typography
-              className="titulo-form"
-              variant="h3"
-              gutterBottom
-              component="h3"
-              align="center"
-            >
+            <Typography className="titulo-form" variant="h3" gutterBottom component="h3" align="center">
               Entrar
             </Typography>
-            <TextField
-              id="usuario"
-              label="usuário"
-              variant="outlined"
-              name="usuario"
-              margin="normal"
-              fullWidth
-            />
-            <TextField
-              id="senha"
-              label="senha"
-              variant="outlined"
-              name="senha"
-              margin="normal"
-              type="password"
-              fullWidth
-            />
+            <TextField id="usuario" label="usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
+            <TextField id="senha" label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
             <Box marginTop={2} textAlign="center">
               <Link to="/home" className="link-login">
-                <Button
-                  className="botao-enviar"
-                  type="submit"
-                  variant="contained"
-                >
+                <Button className="botao-enviar" type="submit" variant="contained" >
                   Logar
                 </Button>
               </Link>
@@ -56,32 +25,18 @@ function Login() {
           </form>
           <Box display="flex" justifyContent="center" marginTop={3}>
             <Box marginRight={1}>
-              <Typography
-                variant="subtitle1"
-                gutterBottom
-                align="center"
-                className="legenda-login"
-              >
+              <Typography variant="subtitle1" gutterBottom align="center" className="legenda-login" >
                 Não tem uma conta?
               </Typography>
             </Box>
-            <Typography
-              variant="subtitle1"
-              gutterBottom
-              align="center"
-              className="legenda-cadastrar"
-            >
-              <Link to="/cadastro">Cadastre-se</Link>
+            <Typography variant="subtitle1" gutterBottom align="center" >
+              <Link className="legenda-cadastrar" to="/cadastro">Cadastre-se</Link>
             </Typography>
           </Box>
         </Box>
       </Grid>
       <Grid xs={5} className="img">
-        <img
-          className="img-principal"
-          src="./src/assets/astronauta.png"
-          alt=""
-        />
+        <img className="img-principal" src="./src/assets/astronauta.png" alt=""/>
       </Grid>
     </Grid>
   );
