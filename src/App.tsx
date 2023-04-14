69856cc7d91635ab1e661ece09257cc6ce072566
am3rco <am3rco@gmail.com>
2023-04-14 13:48:24 -0300

    Aplicando o Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>Teste</h1>
-    <h2>Teste</h2>
-    <h3>Teste</h3>
+      <h1>Teste</h1>
+      <h2>Teste</h2>
+      <h3>Teste</h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
