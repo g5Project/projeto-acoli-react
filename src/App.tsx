0fdff19743efa595acbeb8e477473f43ea413021
am3rco <am3rco@gmail.com>
2023-04-14 15:00:47 -0300

    Criando a Pagina de Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Home from "./paginas/home/Home";
+import Cadastro from "./paginas/cadastro/Cadastro";
 import Footer from "./components/estaticos/footer/Footer";
-import Login from "./paginas/login/Login"
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from "./paginas/login/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+          <Route path="/cadastro" element={<Cadastro />} />
         </Routes>
       </div>
       <Footer />
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,48 +1,76 @@
-import React from 'react';
+import React from "react";
 
-import './Login.css'
-import { Grid, Typography,TextField, Button } from '@material-ui/core';
-import { Box } from '@mui/material';
-import {Link} from 'react-router-dom'
+import "./Login.css";
+import { Grid, Typography, TextField, Button } from "@material-ui/core";
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Login() {
-    return (
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid xs={6} alignItems="center">
+        <Box paddingX={20}>
+          <form>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              align="center"
+              className="negrito"
+            >
+              Entrar
+            </Typography>
+            <TextField
+              id="usuario"
+              label="Nome do Usuario"
+              variant="outlined"
+              name="usuario"
+              margin="normal"
+              fullWidth
+            ></TextField>
+            <TextField
+              id="senha"
+              label="Digite sua Senha"
+              variant="outlined"
+              name="senha"
+              margin="normal"
+              fullWidth
+              type="password"
+            ></TextField>
 
-            <Grid xs={6} alignItems='center'>
-                <Box paddingX={20}>
-                    <form>
-                        <Typography variant='h3' gutterBottom color='textPrimary' align='center' className='negrito'>Entrar</Typography>
-                        <TextField id='usuario' label='Nome do Usuario' variant='outlined' name='usuario' margin='normal' fullWidth></TextField>
-                        <TextField id='senha' label='Digite sua Senha' variant='outlined' name='senha' margin='normal' fullWidth type='password'></TextField>
+            <Box marginTop={2} textAlign="center">
+              <Link to="/home" className="text-decoration-none">
+                <Button type="submit" variant="contained" className="botao">
+                  Logar
+                </Button>
+              </Link>
+            </Box>
+          </form>
 
-                        <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decoration-none'>
-                                <Button type='submit' variant='contained' className='botao'>
-                                    Logar
-                                </Button>
-                            </Link>
-                        </Box>
-                    </form>
+          <Box display="flex" justifyContent="center" marginTop={2}>
+            <Box marginRight={1}>
+              <Typography variant="subtitle1" gutterBottom align="center">
+                Não possui uma conta?
+              </Typography>
+            </Box>
 
-                    <Box display='flex' justifyContent='center' marginTop={2}>
-                        <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Não possui uma conta?</Typography>
-                        </Box>
+            <Link to="/cadastro" style={{ color: "black" }}>
+              <Typography
+                variant="subtitle1"
+                gutterBottom
+                align="center"
+                className="negrito"
+              >
+                Cadastre-se
+              </Typography>
+            </Link>
+          </Box>
+        </Box>
+      </Grid>
 
-                        <Link to='/cadastro' style={{color: 'black'}}>
-                        <Typography variant='subtitle1'gutterBottom align='center' className='negrito'>Cadastre-se</Typography>
-                        </Link>
-                    </Box>
-                </Box>
-            </Grid>
-
-            <Grid xs={6} className='bg-image'>
-                
-            </Grid>
-        </Grid>
-
-    );
+      <Grid xs={6} className="bg-image"></Grid>
+    </Grid>
+  );
 }
 
 export default Login;
