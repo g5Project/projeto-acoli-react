b69baa7782fccfefcd529076de757b28e70abd0e
Lihsolive <liliamoliveira13@gmail.com>
2023-04-14 14:50:09 -0300

    Implementando rotas e criando pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,29 @@ import { useState } from "react";
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Home from "./paginas/home/Home";
 import Footer from "./components/estaticos/footer/Footer";
+import Login from "./paginas/login/Login"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>
-  <Navbar />
-  <Home />
-  <Footer />
-  </>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          {" "}
+          // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
