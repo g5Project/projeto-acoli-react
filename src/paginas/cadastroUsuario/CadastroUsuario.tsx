20cf9a3fc1afa68b923b6121e012c0617ac3e1f1
am3rco <am3rco@gmail.com>
2023-04-19 11:35:18 -0300

    Implementando a Service

---
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -0,0 +1,142 @@
+import React, { useState, useEffect, ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { Grid, Typography, Button, TextField } from "@material-ui/core";
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import User from "../../models/User";
+import { cadastroUsuario } from "../../services/Service";
+import "./CadastroUsuario.css";
+
+function CadastroUsuario() {
+  let history = useNavigate();
+  const [confirmarSenha, setConfirmarSenha] = useState<String>("");
+  const [user, setUser] = useState<User>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: "",
+  });
+
+  const [userResult, setUserResult] = useState<User>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: "",
+  });
+
+  useEffect(() => {
+    if (userResult.id != 0) {
+      history("/login");
+    }
+  }, [userResult]);
+
+  function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>) {
+    setConfirmarSenha(e.target.value);
+  }
+
+  function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  }
+  async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (confirmarSenha == user.senha) {
+      await cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult);
+      alert("Usuário cadastrado com sucesso");
+    } else {
+      alert("Dados inválidos. Favor verificar as informações de cadastro.");
+    }
+  }
+
+  return (
+    <Grid
+      className="container-cadastro"
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {/* <Grid item xs={6} className="img-cadastro">
+        <img className="img-cadastro" src="./src/assets/foguete.svg" alt="" />
+  </Grid> */}
+      <Grid className="container-form-cadastro" item xs={4} alignItems="center">
+        <Box>
+          <form onSubmit={onSubmit}>
+            <Typography
+              className="titulo-cadastro"
+              variant="h3"
+              gutterBottom
+              component="h3"
+              align="center"
+            >
+              Entrar
+            </Typography>
+            <TextField
+              value={user.nome}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+              id="nome"
+              label="Nome"
+              variant="outlined"
+              name="nome"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              value={user.usuario}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+              id="email"
+              label="Email"
+              variant="outlined"
+              name="usuario"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              value={user.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+              id="senhaCadastro"
+              label="Senha"
+              variant="outlined"
+              name="senha"
+              margin="normal"
+              type="password"
+              fullWidth
+            />
+            <TextField
+              value={confirmarSenha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                confirmarSenhaHandle(e)
+              }
+              id="confirmarSenha"
+              label="Confirmar Senha"
+              variant="outlined"
+              name="confirmarSenha"
+              margin="normal"
+              type="password"
+              fullWidth
+            />
+            <Box marginTop={2} textAlign="center" className="box-bottom">
+              <Link to="/login" className="link-login-cadastro">
+                <Button className="btn-cancelar" variant="contained">
+                  Cancelar
+                </Button>
+              </Link>
+              <Button
+                className="btn-cadastrar"
+                type="submit"
+                variant="contained"
+              >
+                Cadastrar
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
+export default CadastroUsuario;
