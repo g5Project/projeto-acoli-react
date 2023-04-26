82772d6864515e61f7478f580016fcca4d8114c1
am3rco <am3rco@gmail.com>
2023-04-26 11:23:42 -0300

    Arrumando o NavBar

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -6,62 +6,61 @@ import { Box } from "@mui/material";
 import "./Navbar.css";
 
 function Navbar() {
-  function Navbar() {
-    const [token, setToken] = useLocalStorage("token");
-    let navigate = useNavigate();
+  const [token, setToken] = useLocalStorage("token");
+  let navigate = useNavigate();
 
-    function goLogout() {
-      setToken("");
-      alert("Usuário deslogado");
-      navigate("/login");
-    }
-    return (
-      <>
-        <AppBar className="bg-menu">
-          <Toolbar variant="dense" className="container-menu">
-            <Box style={{ cursor: "pointer" }}>
-              <Typography variant="h5" color="inherit">
-                Acoli
-              </Typography>
-            </Box>
-
-            <div>
-              <Box className="menu-links" display="flex">
-                <Link to="/home" className="link-home">
-                  <Box className="menu-item" mx={2}>
-                    <Typography variant="h6" color="inherit">
-                      home
-                    </Typography>
-                  </Box>
-                </Link>
-                <Box className="menu-item" mx={2}>
-                  <Typography variant="h6" color="inherit">
-                    postagens
-                  </Typography>
-                </Box>
-                <Box className="menu-item" mx={2}>
-                  <Typography variant="h6" color="inherit">
-                    temas
-                  </Typography>
-                </Box>
-                <Box className="menu-item" mx={2}>
-                  <Typography variant="h6" color="inherit">
-                    cadastrar tema
-                  </Typography>
-                </Box>
-                <Link to="/login" className="link-logout">
-                  <Box className="menu-item" mx={2}>
-                    <Typography variant="h6" color="inherit">
-                      logout
-                    </Typography>
-                  </Box>
-                </Link>
-              </Box>
-            </div>
-          </Toolbar>
-        </AppBar>
-      </>
-    );
+  function goLogout() {
+    setToken("");
+    alert("Usuário deslogado");
+    navigate("/login");
   }
+
+  return (
+    <>
+      <AppBar className="bg-menu">
+        <Toolbar variant="dense" className="container-menu">
+          <Box style={{ cursor: "pointer" }}>
+            <Typography variant="h5" color="inherit">
+              Acoli
+            </Typography>
+          </Box>
+
+          <div>
+            <Box className="menu-links" display="flex">
+              <Link to="/home" className="link-home">
+                <Box className="menu-item" mx={2}>
+                  <Typography variant="h6" color="inherit">
+                    home
+                  </Typography>
+                </Box>
+              </Link>
+              <Box className="menu-item" mx={2}>
+                <Typography variant="h6" color="inherit">
+                  postagens
+                </Typography>
+              </Box>
+              <Box className="menu-item" mx={2}>
+                <Typography variant="h6" color="inherit">
+                  temas
+                </Typography>
+              </Box>
+              <Box className="menu-item" mx={2}>
+                <Typography variant="h6" color="inherit">
+                  cadastrar tema
+                </Typography>
+              </Box>
+              <Link to="/login" className="link-logout">
+                <Box className="menu-item" mx={2}>
+                  <Typography variant="h6" color="inherit">
+                    logout
+                  </Typography>
+                </Box>
+              </Link>
+            </Box>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
 export default Navbar;
