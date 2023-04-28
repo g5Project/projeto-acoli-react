d526f19c4615c6c249d8288a99b50037b6761cbe
Lihsolive <liliamoliveira13@gmail.com>
2023-04-28 11:48:28 -0300

    Criando o componente DeletarPostagem

---
+++ src/components/postagens/deletarPostagem/DeletarPostagem.tsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+// import { toast } from "react-toastify";
+
+import Postagem from "../../../models/Postagem";
+import { buscaId, deleteId } from "../../../services/Service";
+import { TokenState } from "../../../store/tokens/tokenReducer";
+
+import "./DeletarPostagem.css";
+
+function DeletarPostagem() {
+  let history = useNavigate();
+  const { id } = useParams<{ id: string }>();
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
+  const [post, setPost] = useState<Postagem>();
+
+  useEffect(() => {
+    if (token === "") {
+      alert("Você precisa estar logado!");
+      history("/login");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (id !== undefined) {
+      findById(id);
+    }
+  }, [id]);
+
+  async function findById(id: string) {
+    buscaId(`/postagens/${id}`, setPost, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  async function sim() {
+    history("/posts"); //rota do front-end
+
+    try {
+      await deleteId(`/postagens/${id}`, {
+        //rota do back-end
+        headers: {
+          Authorization: token,
+        },
+      });
+      alert("Postagem deletada com sucesso!");
+    } catch (error) {
+      alert("Erro ao deletar!");
+    }
+  }
+
+  function nao() {
+    history("/posts");
+  }
+
+  return (
+    <>
+      <Box m={2}>
+        <Card variant="outlined">
+          <CardContent>
+            <Box justifyContent="center">
+              <Typography color="textSecondary" gutterBottom>
+                Deseja deletar a Postagem:
+              </Typography>
+              <Typography color="textSecondary">{post?.titulo}</Typography>
+            </Box>
+          </CardContent>
+          <CardActions>
+            <Box display="flex" justifyContent="start" ml={1.0} mb={2}>
+              <Box mx={2}>
+                <Button
+                  onClick={sim}
+                  variant="contained"
+                  className="marginLeft"
+                  size="large"
+                  color="primary"
+                >
+                  Sim
+                </Button>
+              </Box>
+              <Box>
+                <Button
+                  onClick={nao}
+                  variant="contained"
+                  size="large"
+                  color="secondary"
+                >
+                  Não
+                </Button>
+              </Box>
+            </Box>
+          </CardActions>
+        </Card>
+      </Box>
+    </>
+  );
+}
+export default DeletarPostagem;
