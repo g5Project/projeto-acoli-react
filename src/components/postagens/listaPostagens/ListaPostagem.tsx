00b1357b01f080d7bd86c0665fffed0688550ecb
Lihsolive <liliamoliveira13@gmail.com>
2023-04-28 11:49:22 -0300

    Criando o componente ListaPostagem

---
+++ src/components/postagens/listaPostagens/ListaPostagem.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Card, CardActions, CardContent, Button, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
+
+import Postagem from "../../../models/Postagem";
+import { busca } from "../../../services/Service";
+import { TokenState } from "../../../store/tokens/tokenReducer";
+
+import "./ListaPostagem.css";
+
+function ListaPostagem() {
+  let history = useNavigate();
+  const [posts, setPosts] = useState<Postagem[]>([]);
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
+
+  useEffect(() => {
+    if (token == "") {
+      alert('Você precisa estar logado!')
+      history("/login");
+    }
+  }, [token]);
+
+  async function getPost() {
+    await busca("/postagens", setPosts, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  useEffect(() => {
+    getPost();
+  }, [posts.length]);
+
+  return (
+    <>
+      {posts.map((post) => (
+        <Box m={2}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Postagens
+              </Typography>
+              <Typography variant="h5" component="h2">
+                {post.titulo}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {post.descricao}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {post.tema?.descricao}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Box display="flex" justifyContent="center" mb={1.5}>
+                <Link
+                  to={`/formularioPostagem/${post.id}`}
+                  className="text-decorator-none"
+                >
+                  <Box mx={1}>
+                    <Button
+                      variant="contained"
+                      className="marginLeft"
+                      size="small"
+                      color="primary"
+                    >
+                      atualizar
+                    </Button>
+                  </Box>
+                </Link>
+                <Link
+                  to={`/deletarPostagem/${post.id}`}
+                  className="text-decorator-none"
+                >
+                  <Box mx={1}>
+                    <Button variant="contained" size="small" color="secondary">
+                      deletar
+                    </Button>
+                  </Box>
+                </Link>
+              </Box>
+            </CardActions>
+          </Card>
+        </Box>
+      ))}
+    </>
+  );
+}
+
+export default ListaPostagem;
